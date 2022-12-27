1893172699aea15afdf1f6b8f0a8c3bd47ec072c
VadymMintenko <vad.m1ntenko@gmail.com>
2022-12-28 00:58:13 +0200

    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,14 @@
 import Player from '@vimeo/player';
-console.log(Player);
-
-const iframe = document.querySelector('#vimeo-player');
-
+const throttle = require('lodash.throttle');
+// console.log(throttle);
+// console.log(Player);
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+// Подія
+player.on(
+  'timeupdate',
+  throttle(function (currentTime) {
+    localStorage.setItem('seconds', JSON.stringify(currentTime.seconds));
+  }, 1000)
+);
