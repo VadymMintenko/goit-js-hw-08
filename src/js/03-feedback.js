09d64d0c71cfd3eb75f542684a7009356c9a640a
VadymMintenko <vad.m1ntenko@gmail.com>
2022-12-29 20:37:39 +0200

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+const throttle = require('lodash.throttle');
 
+const input = document.querySelector('input');
+const textareaInput = document.querySelector('textarea');
+const form = document.querySelector('.feedback-form');
+
+input.addEventListener('input', throttle(onInput, 500));
+textareaInput.addEventListener('input', throttle(onTextarea, 500));
+form.addEventListener('submit', onSubmit);
+
+const object = {};
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onInput(evt) {
+  object.email = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(object));
+}
+
+function onTextarea(evt) {
+  object.textarea = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(object));
+}
+
+console.log(object);
+
+// console.log(object);
