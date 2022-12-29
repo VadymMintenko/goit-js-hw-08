794552fb578042e99377f31ab6792a5a97967e43
VadymMintenko <vad.m1ntenko@gmail.com>
2022-12-30 00:00:10 +0200

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,42 @@
+// const throttle = require('lodash.throttle');
+
+// const input = document.querySelector('input');
+// const textareaInput = document.querySelector('textarea');
+// const form = document.querySelector('.feedback-form');
+
+// input.addEventListener('input', throttle(onInput, 500));
+// textareaInput.addEventListener('input', throttle(onTextarea, 500));
+// form.addEventListener('submit', onSubmit);
+
+// const object = {};
+
+// function onSubmit(evt) {
+//   evt.preventDefault();
+//   evt.currentTarget.reset();
+//   localStorage.removeItem('feedback-form-state');
+// }
+
+// function onInput(evt) {
+//   object.email = evt.target.value;
+//   localStorage.setItem('feedback-form-state', JSON.stringify(object));
+// }
+
+// function onTextarea(evt) {
+//   object.textarea = evt.target.value;
+//   localStorage.setItem('feedback-form-state', JSON.stringify(object));
+// }
+
+// console.log(object);
+
 const throttle = require('lodash.throttle');
 
-const input = document.querySelector('input');
-const textareaInput = document.querySelector('textarea');
 const form = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('input');
+const textareaEl = document.querySelector('textarea');
 
-input.addEventListener('input', throttle(onInput, 500));
-textareaInput.addEventListener('input', throttle(onTextarea, 500));
+saveEmailValue();
+
+form.addEventListener('input', throttle(onInput, 500));
 form.addEventListener('submit', onSubmit);
 
 const object = {};
@@ -17,15 +48,15 @@ function onSubmit(evt) {
 }
 
 function onInput(evt) {
-  object.email = evt.target.value;
+  object[evt.target.name] = evt.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(object));
 }
 
-function onTextarea(evt) {
-  object.textarea = evt.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(object));
+function saveEmailValue() {
+  const inputValue = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (inputValue) {
+    inputEl.value = inputValue[inputEl.name];
+  }
 }
 
 console.log(object);
-
-// console.log(object);
